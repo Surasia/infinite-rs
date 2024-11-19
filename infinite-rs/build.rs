dd29852832a3d2fd0251f57b43dc7ef2fe9028ba
Surasia <74399067+Surasia@users.noreply.github.com>
2024-11-19 13:13:09 +0100

    build: fix windows linking issue

--- infinite-rs/build.rs
+++ infinite-rs/build.rs
@@ -20,12 +20,7 @@ fn main() {
         println!("cargo:rustc-link-lib=dylib=stdc++");
     } else if cfg!(windows) {
         let cmake_profile = cfg.get_profile();
-        let link_path = format!(
-            "{}/build/bin/CMake/{}/{}",
-            dst.display(),
-            cmake_profile,
-            cmake_profile
-        );
+        let link_path = format!("{}/build/bin/CMake/{}", dst.display(), cmake_profile);
         println!("cargo:rustc-link-search=native={}", link_path);
         println!("cargo:rustc-link-lib=static={}", TARGET_NAME);
     }
